54e7eb8c354b09e4f9ab2cfdaf45bc1174e5a600
alexhe101 <hexuanhua101@gmail.com>
2020-06-25 02:14:47 +0800

    init environment

---
+++ src/test/java/com/code59/caffemall/CaffemallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code59.caffemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaffemallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
