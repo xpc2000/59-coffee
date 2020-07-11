187ef53893df0e090297a92e0fa398417c882ff7
xpc2000 <61380068+xpc2000@users.noreply.github.com>
2020-07-11 10:33:44 +0800

    Add Dao and Services

--- src/main/java/com/code59/caffemall/dao/CollectionDao.java
+++ src/main/java/com/code59/caffemall/dao/CollectionDao.java
@@ -1,9 +1,8 @@
 package com.code59.caffemall.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.code59.caffemall.bean.Order;
+import com.code59.caffemall.bean.Collection;
 import org.springframework.stereotype.Repository;
-
 @Repository
-public interface OrderDao extends BaseMapper<Order> {
+public interface CollectionDao extends BaseMapper<Collection> {
 }
--- src/main/java/com/code59/caffemall/dao/OrderShopDao.java
+++ src/main/java/com/code59/caffemall/dao/OrderShopDao.java
@@ -1,9 +1,9 @@
 package com.code59.caffemall.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.code59.caffemall.bean.Order;
+import com.code59.caffemall.bean.Order_shop;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderDao extends BaseMapper<Order> {
+public interface OrderShopDao extends BaseMapper<Order_shop> {
 }
