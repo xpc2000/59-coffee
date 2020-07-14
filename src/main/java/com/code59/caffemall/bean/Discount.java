262f7ff6c60ef1fbf69f3a91458f2dfa5c9ff72b
xpc2000 <61380068+xpc2000@users.noreply.github.com>
2020-07-14 17:17:33 +0800

    Add files via upload

---
+++ src/main/java/com/code59/caffemall/bean/Discount.java
@@ -0,0 +1,58 @@
+package com.code59.caffemall.bean;
+
+public class Discount {
+    private int payments_1;
+    private int minus_1;
+    private int payments_2;
+    private int minus_2;
+    private int payments_3;
+    private int minus_3;
+
+    public void setPayments_1(int payments_1_){
+        this.payments_1=payments_1_;
+    }
+
+    public int getPayments_1(){
+        return payments_1;
+    }
+
+    public void setMinus_1(int minus_1_){
+        this.minus_1=minus_1_;
+    }
+
+    public int getMinus_1(){
+        return minus_1;
+    }
+
+    public void setPayments_2(int payments_2_){
+        this.payments_2=payments_2_;
+    }
+
+    public int getPayments_2(){
+        return payments_2;
+    }
+
+    public void setMinus_2(int minus_2_){
+        this.minus_2=minus_2_;
+    }
+
+    public int getminus_2(){
+        return minus_2;
+    }
+
+    public void setPayments_3(int payments_3_){
+        this.payments_3=payments_3_;
+    }
+
+    public int getPayments_3(){
+        return payments_3;
+    }
+
+    public void setMinus_3(int minus_3_){
+        this.minus_3=minus_3_;
+    }
+
+    public int getMinus_3(){
+        return minus_3;
+    }
+}
