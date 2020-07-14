566e4755dbcebf92a8a70a24dec24f1cae6728ee
xpc2000 <61380068+xpc2000@users.noreply.github.com>
2020-07-14 17:13:54 +0800

    Add files via upload

--- src/main/java/com/code59/caffemall/service/OrderServices.java
+++ src/main/java/com/code59/caffemall/service/OrderServices.java
@@ -1,16 +1,14 @@
 package com.code59.caffemall.service;
 
-import com.code59.caffemall.bean.Cart;
-import com.code59.caffemall.bean.Order;
-import com.code59.caffemall.bean.Order_detail;
+import com.code59.caffemall.bean.*;
 
 import java.util.List;
 
 public interface OrderServices {
-    public Order add(List<Cart> cartList ,String id_shop);
-    public int update(Order order);
+    public Order_shop add(List<Cart> cartList , String id_shop, Guest guest, Discount discount);
+    public int update(Order_shop order);
     public int delete(String id);
-    public Order get(String id);
+    public Order_shop get(String id);
     public List<Order_detail> show(String id);
 
 }
