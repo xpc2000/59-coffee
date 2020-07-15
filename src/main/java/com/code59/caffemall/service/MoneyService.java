b08fe593dd9ee0791918a52876387f463581ed86
xpc2000 <61380068+xpc2000@users.noreply.github.com>
2020-07-15 11:25:53 +0800

    Add files via upload

--- src/main/java/com/code59/caffemall/service/MoneyService.java
+++ src/main/java/com/code59/caffemall/service/MoneyService.java
@@ -1,6 +1,9 @@
 package com.code59.caffemall.service;
 
 import com.code59.caffemall.bean.Discount;
+import com.code59.caffemall.bean.Order;
+
+import java.util.List;
 
 public interface MoneyService {
     public int set_single_discount(double discount, String id_food);
@@ -8,5 +11,5 @@ public interface MoneyService {
                                         int payments1, int minus1,
                                         int payments2, int minus2,
                                         int payments3, int minus3);
-    public void moneyflow(String start_time,String end_time);
+    public List<Order> moneyflow(String start_time, String end_time);
 }
